36028dcc54f99d001e7414422c45f52e3cdc4e59
Branon Palma <baph2492003@gmail.com>
2022-08-30 17:57:19 -0600

    Funciones DELETE y DOWNLOAD | Brandon Palma | baph2492003@gmail.com

--- src/rutas/backend.routes.js
+++ src/rutas/backend.routes.js
@@ -7,5 +7,7 @@ var api = express.Router();
 ////////////////////////////////////////////////////////////////////////////////
 
 api.get('/listDataBucket', requester.listData);
+api.get('/downloadData/:fileName', requester.descargarData);
+api.delete('/elimarData/:fileName', requester.elimarData);
 
 module.exports = api;
