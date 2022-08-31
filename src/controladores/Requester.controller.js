c5fafe1703d4257015074f2931b63417cf4b3395
OASdL <112503247+OASdL@users.noreply.github.com>
2022-08-31 14:51:04 -0600

    Login, Register, Default Admin
    Omar Salazar - OSalazarPracticas@gmail.com

--- src/controladores/Requester.controller.js
+++ src/controladores/Requester.controller.js
@@ -1,6 +1,8 @@
 require("dotenv").config()
 const Amazon = require('aws-sdk')
-const temporal = new Amazon.S3({ accessKeyId: process.env.ACCESS, secretAccessKey: process.env.SECRET })
+const temporal = new Amazon.S3({ accessKeyId: process.env.ACCESS, secretAccessKey: process.env.SECRET, region: process.env.REGION })
+
+////////////////////////////////////////////////
 
 function listData(req, res) {
     temporal.listObjectsV2({ Bucket: process.env.BUCKET }, (error, archivos) => {
@@ -10,10 +12,37 @@ function listData(req, res) {
     });
 }
 
+////////////////////////////////////////////////
 
+//Descargar
+const descargarData = (req,res) => {
+    const fileName = req.params.fileName;
+    temporal.getObject({Bucket: process.env.BUCKET, Key: fileName},(err,file)=>{
+        if(err) return res.send({err});
+
+        res.send(file.Body);
+    })
+};
+
+//Eliminar
+const elimarData = (req, res) =>{
+    const fileName = req.params.fileName;
+    temporal.deleteObject({Bucket: process.env.BUCKET, Key: fileName},(err,file)=>{
+        if(err) return res.send({err});
+
+        res.send("Documento Eliminado")
+    })
+} 
+
+function uploadData(req, res){
+    res.send('Archivo subido exitosamente: ' + req.file.location)
+}
 
 
 
 module.exports = {
-    listData
+    listData,
+    descargarData,
+    elimarData,
+    uploadData
 }
--- src/rutas/backend.routes.js
+++ src/rutas/backend.routes.js
@@ -1,11 +1,40 @@
 const express = require('express');
 const requester = require('../controladores/Requester.controller');
+const admin = require('../controladores/Admin.controller');
 
 var api = express.Router();
 
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
+const aws = require('aws-sdk')
+const multer = require('multer')
+const multerS3 = require('multer-s3');
+
+const BUCKET = process.env.BUCKET
+const s3 = new aws.S3();
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        acl: "public-read",
+        bucket: BUCKET,
+        key: function (req, file, cb) {
+            console.log(file);
+            cb(null, file.originalname)
+        }
+    })
+})
+
 api.get('/listDataBucket', requester.listData);
+
+api.post('/login', admin.Login);
+api.post('/registro', admin.Register);
+api.put('/newAdmin/:idReq', admin.addAdmin);
+api.put('/oldRequester/:idAdm', admin.removeAdmin);
+
+api.get('/downloadData/:fileName', requester.descargarData);
+api.delete('/elimarData/:fileName', requester.elimarData);
+api.post('/uploadDataBucket', upload.single('file'), requester.uploadData);
 
 module.exports = api;
